526edd58aa369213a138b1e05a580361512c24cf
Donato Bernacchi <dona04122@gmail.com>
2023-11-14 19:14:41 -0300

    first commit

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
-const tasas = //Actualizadas lo mas posible :)
-{
-    "USD Oficial": 368.60,
-    "USD Blue": 970,
-    "USD Turista": 735.76,
-  };
-  
-  function cambio(monedaBase, cantidad, moneda) {
-    if (tasas[moneda]) { 
-      return cantidad / tasas[moneda];
-    } else {
-      return "Moneda no soportada";
-    }
-  }
-  
-  function cambioATodos(monedaBase, cantidad) {
-    const monedas = ["USD Oficial", "USD Blue", "USD Turista"];
-  
-    const conversion = monedas.map((moneda) => { 
-      return {
-        moneda: moneda, 
-        cantidad: cambio (monedaBase, cantidad, moneda), 
-      };
-    });
-  
-    return conversion;
-  }
-  
-  const pesos = "ARS";
-  const userInput = prompt("Ingresa la cantidad de pesos argentinos que deseas convertir:");
-  
-  const cantidad = parseFloat(userInput);
-  
-  if (isNaN(cantidad)) {
-    console.log("Entrada no válida. Por favor, ingresa un número válido.");
+const tasas = {
+  "USD Oficial": 368.60,
+  "USD Blue": 970,
+  "USD Turista": 735.76,
+};
+
+function cambio(monedaBase, cantidad, moneda) {
+  if (tasas[moneda]) {
+    return cantidad / tasas[moneda];
   } else {
-    const conversion = cambioATodos(pesos, cantidad);
-  
-    console.log(`Convertir ${cantidad.toFixed(2)} ${pesos} a:`);
+    return "Moneda no soportada";
+  }
+}
+
+function cambioATodos(monedaBase, cantidad) {
+  const monedas = ["USD Oficial", "USD Blue", "USD Turista"];
+
+  return monedas.map((moneda) => {
+    return {
+      moneda: moneda,
+      cantidad: cambio(monedaBase, cantidad, moneda),
+    };
+  });
+}
+
+function convertir() {
+  const cantidadInput = document.getElementById('cantidad');
+  const resultDiv = document.getElementById('result');
+
+  const cantidad = parseFloat(cantidadInput.value);
+
+  if (isNaN(cantidad)) {
+    resultDiv.innerHTML = "Entrada no válida. Por favor, ingresa un número válido.";
+  } else {
+    const conversion = cambioATodos("ARS", cantidad);
+
+    resultDiv.innerHTML = `<p>Convertir ${cantidad.toFixed(2)} ARS a:</p>`;
     conversion.forEach((conversion) => {
-      console.log(`${conversion.cantidad.toFixed(2)} ${conversion.moneda}`);
+      resultDiv.innerHTML += `<p>${conversion.cantidad.toFixed(2)} ${conversion.moneda}</p>`;
     });
   }
-  
+}
